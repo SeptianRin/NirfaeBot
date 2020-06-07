2a7e5aa64c3877346a7e4aa00a9fd3903d78ec22
Septian Rinaldi R <septianrinaldi98@gmail.com>
2020-06-08 03:04:15 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login("Zx1yswLTwnb-tFRi9uiTJPG-qn0i1vHU");//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
