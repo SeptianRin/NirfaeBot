9b77ddb5088916f3e43a0047ed54960a06b0c469
septianrin <septianrinaldi98@gmail.com>
2020-07-03 23:26:37 +0700

    fixin cat command

--- commands/cat.js
+++ commands/cat.js
@@ -8,14 +8,14 @@ module.exports = {
     let msg = await message.channel.send("Generating...");
 
     let { body } = await superagent.get("https://some-random-api.ml/img/cat");
-    let { deskripsi } = await superagent.get("https://some-random-api.ml/facts/cat")
+    let { deskripsi } = await superagent.get("https://some-random-api.ml/facts/cat");
     if (!{ body }) return message.channel.send("Bot Broke... nanya mulu sih");
 
     let cEmbed = new Discord.RichEmbed()
       .setAuthor("Nirfaedah & Ｅｓｔｅｔｉｋａ", message.guild.iconURL)
       .setImage(body.link)
       .setTimestamp()
-      .setDescription(body.fact)
+      .setDescription(deskripsi.fact)
       .setFooter("bot commie");
 
     message.channel.send({ embed: cEmbed });
