f35138aee65e9411713539bbca8ad708ed694f36
septianrin <septianrinaldi98@gmail.com>
2020-07-03 23:24:28 +0700

    fix cat command

--- commands/cat.js
+++ commands/cat.js
@@ -7,13 +7,15 @@ module.exports = {
   async execute(message, args) {
     let msg = await message.channel.send("Generating...");
 
-    let { body } = await superagent.get("http://aws.random.cat/meow");
+    let { body } = await superagent.get("https://some-random-api.ml/img/cat");
+    let { deskripsi } = await superagent.get("https://some-random-api.ml/facts/cat")
     if (!{ body }) return message.channel.send("Bot Broke... nanya mulu sih");
 
     let cEmbed = new Discord.RichEmbed()
       .setAuthor("Nirfaedah & Ｅｓｔｅｔｉｋａ", message.guild.iconURL)
-      .setImage(body.file)
+      .setImage(body.link)
       .setTimestamp()
+      .setDescription(body.fact)
       .setFooter("bot commie");
 
     message.channel.send({ embed: cEmbed });
