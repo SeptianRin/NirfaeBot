ab37c05f6b973f309d0b8c1cd8c7b6ec298f3e5c
septianrin <septianrinaldi98@gmail.com>
2021-05-04 13:12:09 +0700

    fixing login token

--- bot.js
+++ bot.js
@@ -105,4 +105,4 @@ client.on("message", (message) => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login("process.env.TOKEN"); //BOT_TOKEN is the Client Secret
+client.login(process.env.TOKEN); //BOT_TOKEN is the Client Secret
