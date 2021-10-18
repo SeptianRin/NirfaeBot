ace93d2ce923a4d797280bd1dce3e84cb7325696
Septian Rinaldi Rianggoro <septian.rianggoro@ist.id>
2021-10-18 16:24:17 +0700

    fixin non authorize user

--- commands/ciduk.js
+++ commands/ciduk.js
@@ -9,7 +9,10 @@ module.exports = {
     if (
       !(message.member.roles.cache.some(r=>["Ketua Program Pendidikan Estetika","Ketua Program Pendidikan Nirfaedah", "Dekan Fakultas","Wakil Dekan Fakultas"].includes(r.name)))
     )
-      return message.channel.send("Kamu siapa? beraninya menyuruhku.");
+      setTimeout(() => {
+        member.roles.remove(['705727738383958066']).then(console.log).catch(console.error);
+      }, 2 * 60 * 1000);
+      return message.channel.send(`Kamu bukan orang yang bisa memerintahku ${message.author}, diamlah dan berhenti mengoceh sekarang!`);
 
     if (!String(args[0]).startsWith("<@"))
       return message.channel.send(
